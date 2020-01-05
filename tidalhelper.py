7f54b371065299e408aa4ce0a1af698cd331556b
Isaac Pearl <pearl.isaac@gmail.com>
2020-01-05 01:56:46 -0500

    basic python os file/directory manipulation, dummy .wav files in test_samples folder

--- tidalhelper.py
+++ tidalhelper.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 import sys
+import os
 
-for arg in sys.argv:
-    print(arg)
+def main():
+    path = os.getcwd()
+
+    if os.path.isdir(sys.argv[1]):
+        print('argument is a directory')
+    else:
+        print('argument is not a directory')
+
+    new_folder_path = path+'/testfolder'
+
+    for arg in sys.argv:
+        print('arg: %s', arg)
+
+    try:
+        os.mkdir(new_folder_path)
+    except OSError:
+        print('creation of directory %s failed', new_folder_path)
+    else:
+        print('successfully created directory %s', new_folder_path)
+
+main()
