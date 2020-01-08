e10746dbd0769b3019512b0302cfe11efc861c7f
Isaac Pearl <pearl.isaac@gmail.com>
2020-01-07 23:20:21 -0500

    basic copying of samples into prefix-named folders

--- tidalhelper.py
+++ tidalhelper.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+from shutil import copyfile
 
 def test_file_system():
     path = os.getcwd()
@@ -22,24 +23,43 @@ def test_file_system():
     else:
         print('successfully created directory %s', new_folder_path)
 
-def make_folders(d):
-    folders = {}
-    d.sort()
-    for i in range(len(d)):
-        prefix = d[i].split('_')[0] #assume all files start with desired folder name, then underscore  
+def organize(files):
+    folders = {} #key = prefix, value = list of filenames
+    files.sort()
+    for i in range(len(files)):
+        prefix = files[i].split('_')[0] #assume all files start with desired folder name, then underscore  
         if prefix in folders:
-            folders[prefix].append(d[i])
+            folders[prefix].append(files[i])
         else:
-            folders[prefix] = [d[i]]
+            folders[prefix] = [files[i]]
     return folders
 
+def create_folder(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
+def make_folders(path, folders):
+    curr_path = path + "tidal_samples/"
+    create_folder(curr_path)
+    for prefix in folders:
+        group_path = curr_path+prefix+'/'
+        create_folder(group_path)
+        for sample in folders[prefix]:
+            #print("path+sample: %s, group_path: %s" % (path+sample, group_path+sample))
+            copyfile(path+sample, group_path+sample)
+
 def main():
-    sample_folder = './test_samples' #just for early testing
+    samples_path = './test_samples/' #just for early testing
     if len(sys.argv) > 1:
         if os.path.isdir(sys.argv[1]):
-            sample_folder = sys.argv[1]
+            samples_path = sys.argv[1]
     f = []
-    for(dirpath, dirnames, filenames) in os.walk(sample_folder):
+    for(dirpath, dirnames, filenames) in os.walk(samples_path):
         f.extend(filenames)
-    sample_folders_dict = make_folders(f)
+    folder_info = organize(f)
+    make_folders(samples_path, folder_info)
 main()
