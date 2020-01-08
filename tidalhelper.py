ea39f756c8ff60318ee51297e65c8d92f62713f0
Isaac Pearl <pearl.isaac@gmail.com>
2020-01-07 21:53:39 -0500

    creates dict with prefix key/filename vals

--- tidalhelper.py
+++ tidalhelper.py
@@ -23,18 +23,23 @@ def test_file_system():
         print('successfully created directory %s', new_folder_path)
 
 def make_folders(d):
+    folders = {}
     d.sort()
-    
-    return d 
+    for i in range(len(d)):
+        prefix = d[i].split('_')[0] #assume all files start with desired folder name, then underscore  
+        if prefix in folders:
+            folders[prefix].append(d[i])
+        else:
+            folders[prefix] = [d[i]]
+    return folders
 
 def main():
-    sample_folder = './test_samples'
+    sample_folder = './test_samples' #just for early testing
     if len(sys.argv) > 1:
         if os.path.isdir(sys.argv[1]):
             sample_folder = sys.argv[1]
     f = []
     for(dirpath, dirnames, filenames) in os.walk(sample_folder):
         f.extend(filenames)
-    sample_folders = make_folders(f)
-
+    sample_folders_dict = make_folders(f)
 main()
